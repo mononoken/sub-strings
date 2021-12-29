f12feb6d9806a932c71b73079166760faa842013
mononoken <ken.maeshima@pm.me>
2021-12-29 07:32:18 -0600

    find_words_substrings will find all substrings starting from first letter. Needs to cycle through letters now.

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,24 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(string, dictionary)
+test_string = "Howdy partner, sit down! How's it going?"
+
+def substrings(input_string, dictionary)
+  words_array = input_string.downcase.split
 
 end
 
+def find_words_substrings(word)
+  letters_array = word.split("")
+  first_letter_pos = 0
+  last_letter_pos = letters_array.length - 1
+  sub_str_array = []
+
+  until last_letter_pos <= first_letter_pos
+    sub_str = letters_array[first_letter_pos,last_letter_pos]
+    sub_str_array.push(sub_str.join(""))
+    last_letter_pos -= 1
+  end
+  sub_str_array
+end
+
+substrings(test_string, dictionary)
